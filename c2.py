6d3c5d95f3e4f9cb40c2fa3de30337cb4d800f23
vvladdaa <martuniukvladislava@gmail.com>
2025-08-21 01:15:12 +0300

    c2.py

--- c2.py
+++ c2.py
@@ -1,51 +1,51 @@
-# numbers = input("Enter numbers: ").split()
-# numbers = [int(num) for num in numbers]
-# count = 0
-# for i in range(1, len(numbers)):
-#     if numbers[i] > numbers[i-1]:
-#         count += 1
-# print(f"Number of elements, greater than the previous one: {count}")
+numbers = input("Enter numbers: ").split()
+numbers = [int(num) for num in numbers]
+count = 0
+for i in range(1, len(numbers)):
+    if numbers[i] > numbers[i-1]:
+        count += 1
+print(f"Number of elements, greater than the previous one: {count}")
 
-# numbers1 = input("Enter numbers: ").split()
-# numbers1 = [int(num) for num in numbers1]
-# count = {}
-# for num in numbers1:
-#     if num in count:
-#         count[num] += 1
-#     else:
-#         count[num] = 1
-# unique = [num for num in numbers1 if count[num] == 1]
-# print(f"Elements that appear only once: {unique}")
+numbers1 = input("Enter numbers: ").split()
+numbers1 = [int(num) for num in numbers1]
+count = {}
+for num in numbers1:
+    if num in count:
+        count[num] += 1
+    else:
+        count[num] = 1
+unique = [num for num in numbers1 if count[num] == 1]
+print(f"Elements that appear only once: {unique}")
 
-# numbers2 = input("Enter numbers: ").split()
-# numbers2 = [int(num) for num in numbers2]
-# max = 1
-# curr = 1
-# max1 = [numbers2[0]]
-# curr1 = [numbers2[0]]
-# for i in range(1, len(numbers2)):
-#     if numbers2[i] > numbers2[i-1]:
-#         curr += 1
-#         curr1.append(numbers2[i])
-#     else:
-#         if curr > max:
-#             max = curr
-#             max1 = curr1.copy()
-#         curr = 1
-#         curr1 = [numbers2[i]]
-# if curr > max:
-#     max = curr
-#     max1 = curr1.copy()
-# print(f"Length: {max}")
-# print(f"Sequence: {max1}")
+numbers2 = input("Enter numbers: ").split()
+numbers2 = [int(num) for num in numbers2]
+max = 1
+curr = 1
+max1 = [numbers2[0]]
+curr1 = [numbers2[0]]
+for i in range(1, len(numbers2)):
+    if numbers2[i] > numbers2[i-1]:
+        curr += 1
+        curr1.append(numbers2[i])
+    else:
+        if curr > max:
+            max = curr
+            max1 = curr1.copy()
+        curr = 1
+        curr1 = [numbers2[i]]
+if curr > max:
+    max = curr
+    max1 = curr1.copy()
+print(f"Length: {max}")
+print(f"Sequence: {max1}")
 
-# numbers4 = input("Enter numbers: ").split()
-# numbers4 = [int(num) for num in numbers4]
-# N = int(input("Enter the number of positions to shift right: "))
-# N = N % len(numbers4)
-# shift = numbers4[-N:] + numbers4[:-N]
-# print(f"Original list: {numbers4}")
-# print(f"Shifted list: {shift}")
+numbers4 = input("Enter numbers: ").split()
+numbers4 = [int(num) for num in numbers4]
+N = int(input("Enter the number of positions to shift right: "))
+N = N % len(numbers4)
+shift = numbers4[-N:] + numbers4[:-N]
+print(f"Original list: {numbers4}")
+print(f"Shifted list: {shift}")
 
 import random
 list11 = [random.randint(1, 20) for _ in range(10)]
@@ -87,3 +87,20 @@ while True:
         break
     else:
         print("Помилка! Будь ласка, оберіть число від 0 до 6.")
+
+numbers6 = input("Введіть цілі числа, розділені пробілами: ").split()
+numbers6 = [int(num) for num in numbers6]
+sorted6 = sorted(numbers6)
+result6 = []
+left6 = 0
+right6 = len(sorted6) - 1
+while left6 <= right6:
+    if left6 == right6:
+        result6.append(sorted6[left6])
+    else:
+        result6.append(sorted6[left6])
+        result6.append(sorted6[right6])
+    left6 += 1
+    right6 -= 1
+print(f"Початковий список: {numbers6}")
+print(f"Відсортований список з чергуванням: {result6}")
